b8c1a679023ec858708360204c7d37188095ef69
Terchiwassim <131512997+Terchiwassim@users.noreply.github.com>
2024-05-18 18:25:28 -0700

    Add files via upload

---
+++ projectdone/components/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact Us.</h3>
+         <a href="tel:9800000000"><i class="fas fa-phone"></i> +213 7 74 47 15 75</a>
+         <a href="tel:9900000000"><i class="fas fa-phone"></i> +213 5 51 33 01 08</a>
+         <a href="nedjmafares01@gmail.com"><i class="fas fa-envelope"></i> djelfa.mssad@gmail.com</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> algeria,djelfa</a>
+      </div>
+
+      <div class="box">
+         <h3>Follow Us</h3>
+         <a href="https://www.facebook.com/profile.php?id=100077298182051&locale=ar_AR" target="_blank"><i class="fab fa-facebook-f"></i>facebook</a>
+         
+         <a href="https://www.instagram.com/wassi_iiim/" target="_blank"><i class="fab fa-instagram"></i>Instagram</a>
+        
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>nedjma /tourchi</span> | all rights reserved!</div>
+
+</footer>
